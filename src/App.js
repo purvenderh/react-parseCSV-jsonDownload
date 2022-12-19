a603a12c0ce87654965853361e76e21138d6e2c1
purvenderh <purvender@superclub.xyz>
2022-12-19 21:03:30 +0530

    csv2json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import "./App.css";
-import { Button } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
 import { useCSVReader } from "react-papaparse";
 import { useState } from "react";
-import Typography from "@mui/material/Typography";
 
 function App() {
   const { CSVReader } = useCSVReader();
-  const [column, setColumn] = useState();
   const [row, setRow] = useState();
+
+  const exportData = () => {
+    var educationList = [];
+    for (let i = 1; i <= 10; i++) {
+      if (!row["education_" + i]) break;
+      educationList.push({
+        school: row["education_"+i],
+        degree: row["education_degree_"+i],
+        speciality: row["education_fos_"+i],
+        description: row["education_description_"+i],
+        startDate: new Date(row["education_start_"+i]),
+        endDate: row["education_end_"+i]?new Date(row["education_end_"+i]):"",
+        position: i,
+      });
+    }
+
+    var employmentList = [];
+
+    for (let i = 1; i <= 10; i++) {
+      if (!row["organization_" + i]) break;
+      employmentList.push({
+        org_name: row["organization_" + i],
+        org_linkedin_id: row["organization_id_" + i],
+        designation: row["organization_title_" + i],
+        description: row["organization_description_" + i],
+        startDate: new Date(row["organization_start_" + i]),
+        endDate: row["organization_end_" + i]?new Date(row["organization_end_" + i]):"",
+        location: row["organization_location_" + i],
+        position: i,
+      });
+    }
+    
+    var result = {};
+    result.educationList = educationList;
+    result.employmentList = employmentList;
+    console.log(result)
+  };
 
   const convertJson = (res) => {
     const rawData = res.data;
@@ -17,7 +50,7 @@ function App() {
     const colArray = rawData.shift();
 
     //get array of objects from array of array for datagrid
-    const objData = res.data.map((row, index) => {
+    const objData = rawData.map((row, index) => {
       //each object with key as column
       const outObj = {};
 
@@ -27,39 +60,7 @@ function App() {
       });
       return outObj;
     });
-
-    //get array of objects for datagrid column and initializing it with width and headerName
-    const colObjForTab = colArray.map((colName) => {
-      return {
-        field: colName,
-        headerName: colName.toUpperCase(),
-        minWidth: 100,
-        flex: 1,
-      };
-    });
-
-    //updating columns and rows for tab
-    setColumn(colObjForTab);
-    setRow(objData);
-  };
-
-  const removeCSV = () => {
-    setRow(null);
-    setColumn(null);
-  };
-
-  //function for exporting json data
-  const exportData = () => {
-    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
-      JSON.stringify(row)
-    )}`;
-
-    //creating link , adding href and download and click property
-    const link = document.createElement("a");
-    link.href = jsonString;
-    link.download = "data.json";
-
-    link.click();
+    setRow(objData[0]);
   };
 
   //styles for crv converter
@@ -72,102 +73,38 @@ function App() {
       margin: "auto",
       maxWidth: "400px",
     },
-    browseFile: {
-      width: "20%",
-    },
     acceptedFile: {
       border: "1px solid #ccc",
       height: 45,
       lineHeight: 2.5,
       paddingLeft: 10,
       width: "80%",
-    },
-    remove: {
-      borderRadius: 0,
-      padding: "0 20px",
-    },
-    progressBarBackgroundColor: {
-      backgroundColor: "red",
-    },
+    }
   };
 
   return (
     <div className="App" style={{ marginTop: "3rem", marginBottom: "3rem" }}>
-      {row && (
-        <Button
-          variant="contained"
-          color="success"
-          onClick={exportData}
-          style={{ marginBottom: "3rem" }}
-        >
-          Download Json
-        </Button>
-      )}
-
       <CSVReader
         onUploadAccepted={(results: any) => {
           convertJson(results);
         }}
       >
-        {({
-          getRootProps,
-          acceptedFile,
-          ProgressBar,
-          getRemoveFileProps,
-        }: any) => (
+        {({ getRootProps, acceptedFile }: any) => (
           <>
             <div style={styles.csvReader}>
-              <button type="button" {...getRootProps()}>
-                Browse file
-              </button>
+              {!row && (
+                <button type="button" {...getRootProps()}>
+                  Browse file
+                </button>
+              )}
+              {row && <button onClick={exportData}>Upload Data</button>}
               <div style={styles.acceptedFile}>
                 {acceptedFile && acceptedFile.name}
               </div>
-              {row && (
-                <Button
-                  variant="contained"
-                  color="error"
-                  onClick={removeCSV}
-                  // {...getRemoveFileProps()}
-                  style={styles.remove}
-                >
-                  Remove
-                </Button>
-              )}
             </div>
-            <ProgressBar style={styles.progressBarBackgroundColor} />
           </>
         )}
       </CSVReader>
-
-      <div
-        style={{
-          height: 400,
-          width: "100%",
-          marginTop: "3rem",
-          marginBottom: "3rem",
-        }}
-      >
-        {row && (
-          <DataGrid
-            rows={row}
-            columns={column}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            disableSelectionOnClick
-          />
-        )}
-      </div>
-
-      <div>
-        <Typography variant="h6" gutterBottom component="div" color="success">
-          CSV parser Source:-{" "}
-          <a href="https://react-papaparse.js.org/">React papa-parse</a>
-        </Typography>
-        <Typography variant="h6" gutterBottom component="div" color="success">
-          Data Source:- <a href="https://www.mockaroo.com/">Mockaroo</a>
-        </Typography>
-      </div>
     </div>
   );
 }
